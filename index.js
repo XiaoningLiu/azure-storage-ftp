2b2d3af390f8ebfae0d9c5d59d8bba7f6f061d2b
Joi Ge <yuge@microsoft.com>
2017-07-25 17:03:04 +0800

    Get function v2 - GYJ

--- index.js
+++ index.js
@@ -11,6 +11,28 @@ const FtpServer = require('ftp-srv');
 const FileSystem = FtpServer.FileSystem;
 const thenify = require('thenify');
 
+var storageAccount = 'browserifytest';
+var SASToken = '?sv=2015-12-11&ss=bfqt&srt=sco&sp=rwdlacup&se=2017-12-27T22:19:10Z&st=2016-12-27T14:19:10Z&spr=https&sig=IHhkdpaB9PkccStZvSSqMxFSA16SMSQwIDFB97XStOY%3D';
+var blobURI = `https://${storageAccount}.blob.core.windows.net`;
+var containerNmae = 'aacontainer';
+// 
+console.log(blobURI);
+var blobService = AzureStorage.createBlobServiceWithSas(blobURI, SASToken);
+function fn(containerNmae, blobName, cb) {
+  blobService.getBlobToText(containerNmae, blobName, function (err, res) {
+    // if (!err) {
+    //   console.log(res);
+    // }
+    test = "abc";
+    cb(err, res, test);
+  });
+}
+const p = thenify(fn);
+// console.log(p);
+p('aacontainer', 'test.txt')
+.then(val => console.log(val))
+.catch(err => console.log(err));
+
 class AzureStorageFileSystem extends FileSystem {
     constructor(connection, { root, cwd, storageAccount, storageSASToken } = {}) {
         super(connection, { root, cwd });
@@ -59,7 +81,7 @@ class AzureStorageFileSystem extends FileSystem {
             })().then(function (values) {
                 // TODO: transform storage returned values into fs.stat like objects (in above method comments)
                 return {
-                    name: 'xxx', // container/blob name
+                    name: 'xxx', // container/blob
                     isDirectory: function () { return true }, // Return true when it's a container
                     dev: 920907695,
                     mode: 16822,
